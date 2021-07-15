32f0c84516f7b5ba80304facfa8ba7db3d4e4635
josezanle <joze44_zero@hotmail.com>
2021-07-15 02:19:24 -0300

    ajusts

--- controllers/avisoController.js
+++ controllers/avisoController.js
@@ -1,94 +1,86 @@
 const Aviso = require("../models/Aviso");
 
-exports.nuevoAviso = async(req,res) => {
-    // crear objeto con info del aviso
-    const aviso = new Aviso(req.body)
-    try {
-        await aviso.save();
-        res.status(200).json({
-            ok:true,
-            mensaje:"Aviso agregado con exito"
-        })
-        
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            mensaje:"No se pudo agregar el aviso"
-        })
-        
-    }
-}
+exports.nuevoAviso = async (req, res) => {
+  // crear objeto con info del aviso
+  const aviso = new Aviso(req.body);
+  try {
+    await aviso.save();
+    res.status(200).json({
+      ok: true,
+      mensaje: "Aviso agregado con exito",
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      mensaje: "No se pudo agregar el aviso",
+    });
+  }
+};
 // ====================================================================
 // get de todos los avisos
 
-exports.getAvisos = async(req,res) => {
-    try {
-        const avisos = await Aviso.find({})
-        res.status(201).json(avisos)
-        
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            mensaje:"No se puede consultar avisos"
-        })
-        
-    }
-}
+exports.getAvisos = async (req, res) => {
+  try {
+    const avisos = await Aviso.find({});
+    res.status(201).json(avisos);
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      mensaje: "No se puede consultar avisos",
+    });
+  }
+};
 //=======================================================================
 
 // get aviso por :id
-exports.getAviso = async(req,res) => {
-    try {
-        const aviso = await Aviso.findById(req.params.id)
-        res.status(201).json(aviso)
-        
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            mensaje:"No se puede consultar aviso por id"
-        })
-        
-    }
-}
+exports.getAviso = async (req, res) => {
+  try {
+    const aviso = await Aviso.findById({_id:req.params.id});
+    res.status(200).json(aviso);
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      mensaje: "No se puede consultar aviso por id",
+    });
+  }
+};
 
 // =======================================================================
 
 // put aviso por :id
-exports.actualizarAviso = async(req,res) => {
-    try {
-        const aviso = await Aviso.findByIdAndUpdate({_id:req.params.id}, req.body,{
-            new:true
-        })
-        res.status(201).json(aviso)
-        
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            mensaje:"No se puede actualizar aviso"
-        })
-        
-    }
-}
+exports.actualizarAviso = async (req, res) => {
+  try {
+    const aviso = await Aviso.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(aviso);
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      mensaje: "No se puede actualizar aviso",
+    });
+  }
+};
 
 // =======================================================================
 
 // delete aviso por :id
-exports.borrarAviso = async(req,res) => {
-    try {
-        const aviso = await Aviso.findByIdAndDelete({_id:req.params.id})
-        res.status(201).json({
-            ok:true,
-            mensaje:"Aviso Borrado"
-        })
-        
-    } catch (error) {
-        res.status(400).json({
-            ok:false,
-            mensaje:"No se pudo eliminar avisos"
-        })
-        
-    }
-}
-
-
-
+exports.borrarAviso = async (req, res) => {
+  const { id } = request.params;
+  try {
+    const aviso = await Aviso.findByIdAndRemove(id);
+    res.status(200).json({
+      ok: true,
+      mensaje: "Aviso Borrado",
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      mensaje: "No se pudo eliminar avisos",
+    });
+  }
+};
