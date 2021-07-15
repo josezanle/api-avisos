90516d3301fc557148e89d6ae01c77b4b19a760f
josezanle <joze44_zero@hotmail.com>
2021-07-15 10:51:48 -0300

    ajusts

--- controllers/avisoController.js
+++ controllers/avisoController.js
@@ -72,7 +72,7 @@ exports.actualizarAviso = async (req, res) => {
 exports.borrarAviso = async (req, res) => {
   const { id } = req.params;
   try {
-    const aviso = await Aviso.findByIdAndDelete({_id:req.params.id});
+    const aviso = await Aviso.findByIdAndDelete({_id:id});
     res.status(200).json(aviso);
   } catch (error) {
     res.status(400).json({
